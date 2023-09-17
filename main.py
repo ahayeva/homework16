9c39608e127b0cc1e176e96df3237cb51130543b
ahayeva <138230094+ahayeva@users.noreply.github.com>
2023-09-18 00:06:46 +0300

    555

--- main.py
+++ main.py
@@ -1,24 +1,41 @@
-x = int(input("Введіть перше число (x): "))
-y = int(input("Введіть друге число (y): "))
-result = x ** y
-print(f"{x} у степені {y} = {result}")
-#exercise2
-count = 0
-for num in range(100, 1000):
-    digits = [int(digit) for digit in str(num)]
-    if len(set(digits)) < 3:
-        count += 1
-print(f"Кількість чисел з двома однаковими цифрами від 100 до 999: {count}")
-#exercise3
-count = 0
-for num in range(100, 10000):
-    digits = [int(digit) for digit in str(num)]
-    if len(set(digits)) == 4:
-        count += 1
-print(f"Кількість чисел з різними цифрами від 100 до 9999: {count}")
-#exercise4
-input_number = int(input("Введіть ціле число: "))
-input_number_str = str(input_number)
-result_str = "".join(char for char in input_number_str if char not in "36")
-result_number = int(result_str)
-print(f"Число після видалення цифр 3 і 6: {result_number}")
+
+expression = input("Введіть арифметичний вираз (наприклад, 23+12): ")
+try:
+    num1, operator, num2 = expression.split()
+    num1 = float(num1)
+    num2 = float(num2)
+except ValueError:
+    print("Неправильний формат виразу")
+    exit(1)
+
+if operator == '+':
+    result = num1 + num2
+elif operator == '-':
+    result = num1 - num2
+elif operator == '*':
+    result = num1 * num2
+elif operator == '/':
+    if num2 == 0:
+        print("Ділення на нуль неможливе")
+        exit(1)
+    result = num1 / num2
+else:
+    print("Неправильний оператор")
+    exit(1)
+
+print(f"Результат: {result}")
+#exercise17
+import random
+
+random_numbers = [random.randint(-100, 100) for _ in range(20)]
+
+min_number = min(random_numbers)
+max_number = max(random_numbers)
+negative_count = sum(1 for num in random_numbers if num < 0)
+positive_count = sum(1 for num in random_numbers if num > 0)
+zero_count = sum(1 for num in random_numbers if num == 0)
+print(f"Мінімальний елемент: {min_number}")
+print(f"Максимальний елемент: {max_number}")
+print(f"Кількість від'ємних елементів: {negative_count}")
+print(f"Кількість додатніх елементів: {positive_count}")
+print(f"Кількість нулів: {zero_count}")
